b87b0f3dc7611da7bf46c43691c17a1e24594cf2
Wilfried71 <tpeyrot71@gmail.com>
2025-04-12 14:22:07 -0400

    Added */!<> in token list

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -60,8 +60,12 @@ func testTokens(input string, tests []TokenTestStruct, t *testing.T) {
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 		let ten = 10;
-		let add = fn(x, y) { x + y;	};
+		let add = fn(x, y) {
+		x + y;
+		};
 		let result = add(five, ten);
+		!-/*5;
+		5 < 10 > 5;
 	`
 
 	tests := []TokenTestStruct{
@@ -100,6 +104,18 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
